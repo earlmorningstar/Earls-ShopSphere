f824ea0a8d49f80a44d996a902e6e59ec40a68a8
Onyeabor Joel <earlmorningstar@gmail.com>
2024-02-02 13:25:10 +0100

    Basic Commit.

--- src/components/DealsLine/HowDoYouWantYourItems.jsx
+++ src/components/DealsLine/HowDoYouWantYourItems.jsx
@@ -1,8 +1,11 @@
-import './DealsLine.css';
+import { MdArrowDropDown } from "react-icons/md";
+import "./DealsLine.css";
 
 export default function HowDoYouWantYourItems() {
-    return (
-        <div className="hdywyi">How do you want your items?!</div>
-    )
+  return (
+    <div className="hdywyiParent">
+      <div className="hdywyi">How do you want your items?</div>
+      <MdArrowDropDown size={20}/>
+    </div>
+  );
 }
-
