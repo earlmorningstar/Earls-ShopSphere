1d99ce1a084da1042aa0017bc1a4c7ba1a37ff26
Onyeabor Joel <earlmorningstar@gmail.com>
2024-02-13 12:35:12 +0100

    Collection Commit.

--- src/components/Collection/CollectionProduct.jsx
+++ src/components/Collection/CollectionProduct.jsx
@@ -1,27 +1,29 @@
 import { IoIosHeartEmpty } from "react-icons/io";
+import { GoPlus } from "react-icons/go";
 import NewFash from "../../assets/NewFash2.png";
 import "./MainCollection.css";
 
 export default function CollectionProduct() {
   return (
     <>
-    <div className="coll-prod-parent">
-      <div className="coll-prod-img">
-        <img src={NewFash} alt="" />
+      <div className="coll-prod-parent">
+        <div className="coll-prod-img">
+          <img src={NewFash} alt="" />
+        </div>
+        <div className="coll-prod-price-star-parent">
+          <span>$44.99</span>
+          <span>
+            <IoIosHeartEmpty size={18} />
+          </span>
+        </div>
+        <div className="coll-prod-info">
+          [Product Info] Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Porro, error! Minus itaque
+        </div>
+        <button>
+          <GoPlus size={15} color="#ffffff" /> Add
+        </button>
       </div>
-      <div className="coll-prod-price-star-parent">
-        <span>$44.99</span>
-        <span>
-          <IoIosHeartEmpty size={18} />
-        </span>
-      </div>
-      <div className="coll-prod-info">
-        [Product Info] Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Porro, error! Minus itaque
-      </div>
-      <button>+ Add</button>
-    </div>
     </>
-    
   );
 }
