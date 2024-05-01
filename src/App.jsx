ca4cac5da2bd7fb539162b0920e4c21548d401ed
Onyeabor Joel <earlmorningstar@gmail.com>
2024-05-01 12:38:43 +0100

    AdsComponent Commit.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
 import { LikedItemsContextProvider } from "./store/LikedItemsContext.jsx";
 
-import AdsLine from "./components/Adsline/Adsline.jsx";
+import AdsComponent from "./components/AdsComponent/AdsComponent.jsx";
 import DealsLine from "./components/DealsLine/Dealsline.jsx";
 import Header from "./components/Header/Header.jsx";
 import MainContentCarousel from "./components/MainContent/MainContentCarousel.jsx";
@@ -17,12 +17,11 @@ import Checkout from "./components/Checkout.jsx";
 import Likes from "./components/Favorited/Likes.jsx";
 
 function App() {
-
   return (
     <UserProgressContextProvider>
       <LikedItemsContextProvider>
         <CartContextProvider>
-          <AdsLine />
+          <AdsComponent />
           <Header />
           <DealsLine />
           <MainContentCarousel />
---
+++ src/components/AdsComponent/AdsComponent.jsx
@@ -0,0 +1,16 @@
+import './AdsComponent.css';
+
+export default function AdsComponent() {
+    return (
+        <div className="adsParent">
+          <div className="ads">
+            <span>ShopSphereYard!</span>
+            <span>Top Purchase Store</span>
+            <span>Top purchase enterprise</span>
+            <span>Purchase with a specialist</span>
+          </div>
+    
+          <div>Hotline: +123 456 789 00</div>
+        </div>
+      );
+}
