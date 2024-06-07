d7545b94eb03117fa3d9abf5cf6638e665a8693e
Onyeabor Joel <earlmorningstar@gmail.com>
2024-06-08 00:26:24 +0100

    Backend Commit.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// // https://vitejs.dev/config/
+// import million from "million/compiler";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+ 
+// export default defineConfig({
+//   plugins: [million.vite({ auto: true }), react()],
+// });
+
+
 // https://vitejs.dev/config/
 import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
- 
+
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
+  build: {
+    outDir: 'dist',
+  },
 });
